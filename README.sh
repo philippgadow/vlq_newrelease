9db8f77368cb701756e7b6757f37f3f71d775fab
Paul Philipp Gadow <paul.philipp.gadow@cern.ch>
2021-02-24 19:42:49 +0100

    add readme

---
+++ README.sh
@@ -0,0 +1,15 @@
+## Development of the VLQ job option for Atlas MC Generation releases 21.6.23 and later
+
+### Run this example quickly
+
+```
+git clone git@github.com:philippgadow/vlq_newrelease.git
+source setup.sh
+source run.sh
+```
+
+### Modify job option
+
+Take the job option [](https://github.com/philippgadow/vlq_newrelease/blob/master/100xxx/100001/mc.MGPy8EG_ZBHb1000LH100_sigonly.py) to implement your changes for testing.
+
+To do: copy this file to `CommonFiles/MGPy8EG_A14NNPDF23LO_SingleVLQ.py ` and modify all job options to symlinks
